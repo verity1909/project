3f387692b83fb1e380deb452346935efcf210f23
Kate <example@mail.com>
2019-09-23 21:38:06 +0300

    first

---
+++ jjava.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var obj = {};
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+//console.log(persone["age"]);
+let arr = ['plum.jpg', 'orange.jpg', 'melon.jpg'] ;
+//console.log(arr[1]);
+
+let answer = prompt("Are you here bitch?", "Yep");
+
+//console.log(typeof(undefined));
+let min = 5,
+maf = 3;
+min++;
+maf--;
+
+console.log(min);
+console.log(maf);
+console.log(7 == 4);
